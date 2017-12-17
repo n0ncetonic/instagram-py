eb1e95c443be9110fb7b924e8720fb5e39aaae79
antony-jr <antonyjr@protonmail.com>
2017-12-17 23:26:36 +0530

    Resurection of Instagram-Py!

---
+++ InstagramPy/AppInfo.py
@@ -0,0 +1,15 @@
+# The MIT License
+# Copyright (C) 2017 Antony Jr.
+#
+# @filename    : AppInfo.py
+# @description : Application Information that are required
+
+appInfo = {
+    "version": "2.0.1",
+    "name": "Instagram-Py",
+    "description": " Slick Instagram brute force command line tool",
+    "author": "antony-jr",
+    "company": "",
+    "year": "2017",
+    "example": "example: instagram-py -c -vvv -u instatestgod__ -pl rockyou.txt"
+}
